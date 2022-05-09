4684793407883f2be14ca7e376ad27904aa18fea
grochalapiotr <miami2042@gmail.com>
2022-05-09 11:34:07 +0200

    first integration for sql product storage

--- src/main/java/pl/pgrochala/crm/InquiryController.java
+++ src/main/java/pl/pgrochala/crm/InquiryController.java
@@ -1,20 +1,23 @@
 package pl.pgrochala.crm;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 @RestController
 public class InquiryController {
     @Autowired
     InquiryCRUD inquiryCRUD;
+    @GetMapping("/api/inquiry")
     public List<Inquiry> all() {
-        return inquiryCRUD.findAll();
+        return  inquiryCRUD.findAll();
     }
 
+    @PostMapping("/api/inquiry")
     public void createInquiry(@RequestBody Inquiry inquiry) {
         inquiryCRUD.save(inquiry);
     }
